b7fb72beb35a9de0b9795e55fe571f93a3c58b2b
eknoes <me@eknoes.de>
2016-11-19 11:02:24 +0100

    Test for autodeploy

--- ean/endpoints/version.py
+++ ean/endpoints/version.py
@@ -4,6 +4,6 @@ from flask_restful import Resource
 class APIInfo(Resource):
     @staticmethod
     def get():
-        return {'api': {'version': 1, 'health': 'good'},
+        return {'api': {'version': 1, 'health': 'online'},
                 'android': {'version': 0}
                 }
