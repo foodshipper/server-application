6476c6154692770bdf1e965cb807aae1932b97c1
eknoes <me@eknoes.de>
2016-11-19 13:32:25 +0100

    Added /home-location Endpoint

--- ean/api.py
+++ ean/api.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from ean.endpoints.products import Product, ProductTypes
 from ean.endpoints.version import APIInfo
 from ean.endpoints.fridge_items import FridgeOverview, FridgeItem
+from ean.endpoints.home_location import HomeLocation
 
 api = Api()
 
@@ -12,3 +13,4 @@ api.add_resource(ProductTypes, '/v1/types')
 api.add_resource(APIInfo, '/status')
 api.add_resource(FridgeOverview, '/v1/items')
 api.add_resource(FridgeItem, '/v1/items/<string:ean>')
+api.add_resource(HomeLocation, '/v1/home-location')
