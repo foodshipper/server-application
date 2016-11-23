def4295a3ddfe2be363f5addfb5e87a5a1eeeac9
eknoes <me@eknoes.de>
2016-11-23 22:59:23 +0100

    Fixing Unicode Issue

--- ean/wsgi.py
+++ ean/wsgi.py
@@ -1,7 +1,10 @@
+import sys
+
 from ean.app import create_app
 from werkzeug.contrib.fixers import ProxyFix # needed for http server proxies
 from werkzeug.debug import DebuggedApplication
 
+print("Use Python Version " + sys.version_info)
 app = create_app()
 app.wsgi_app = ProxyFix(app.wsgi_app) # needed for http server proxies
 application = DebuggedApplication(app, True)
