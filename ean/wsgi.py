f6ae8cbfd93a5e6f314b664bf5d812ed5f4eb181
eknoes <me@eknoes.de>
2016-11-23 23:03:56 +0100

    Fix Issue

--- ean/wsgi.py
+++ ean/wsgi.py
@@ -4,7 +4,7 @@ from ean.app import create_app
 from werkzeug.contrib.fixers import ProxyFix # needed for http server proxies
 from werkzeug.debug import DebuggedApplication
 
-print("Use Python Version " + sys.version_info)
+print("Use Python Version " + sys.version_info.major)
 app = create_app()
 app.wsgi_app = ProxyFix(app.wsgi_app) # needed for http server proxies
 application = DebuggedApplication(app, True)
