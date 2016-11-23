b44f680ff91c96116ca246133781a12dafeb8bc3
eknoes <me@eknoes.de>
2016-11-23 22:50:28 +0100

    Fixing Unicode Issue

--- ean/endpoints/user_name.py
+++ ean/endpoints/user_name.py
@@ -1,3 +1,4 @@
+from circus.py3compat import unicode
 from flask_restful import Resource, abort, reqparse
 from werkzeug.exceptions import BadRequest
 
@@ -25,11 +26,11 @@ class UserName(Resource):
 
                 if id is None:
                     cursor.execute(u"INSERT INTO users (token, name) VALUES (%s, %s)",
-                                   [args['token'], args['name']])
+                                   [args['token'], unicode(args['name'])])
                     return None, 201
                 else:
                     cursor.execute(u"UPDATE users SET name=%s WHERE id=%s",
-                                   [args['name'], id])
+                                   [unicode(args['name']), id])
                     return None, 200
 
     def get(self):
