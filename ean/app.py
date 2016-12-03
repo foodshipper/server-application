47c446609bb1ea0e040760bc8e98f5bcc7ffb729
eknoes <me@eknoes.de>
2016-12-04 00:46:57 +0100

    Added Cron Functionality

--- ean/app.py
+++ ean/app.py
@@ -1,7 +1,22 @@
 from flask import Flask
-
+from crontab import CronTab
+import sys, os
 from ean.api import api
 from ean.database import create_tables
+
+
+def check_cronjob():
+    try:
+        my_cron = CronTab(user=True)
+        cmd = sys.executable + " " + os.getcwd() + "/cron/cron.py"
+        if my_cron.find_command(cmd) is not None:
+            return True
+
+        job = my_cron.new(cmd, 'Foodship API Worker')
+        job.minute.every(15)
+        my_cron.write()
+    except FileNotFoundError as e:
+        print("Could not create Crontab Entry: File not found", file=sys.stderr)
 
 
 def create_app():
@@ -9,6 +24,7 @@ def create_app():
     app.config['ERROR_404_HELP'] = False
     api.init_app(app)
     create_tables()
+    check_cronjob()
 
     return app
 
--- ean/cron/cron.py
+++ ean/cron/cron.py
@@ -1,7 +1,8 @@
 from cron.create_groups import create_groups
 from database import create_tables
 
-def cron():
+
+if __name__ == '__main__':
+    print("Run")
     create_tables()
     create_groups()
-    print("Run")
