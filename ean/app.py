8632d62667fa0bfee9ce7f73a09b2a8e75d8c9c2
eknoes <me@eknoes.de>
2016-12-04 12:10:13 +0100

    Fixing Cron

--- ean/app.py
+++ ean/app.py
@@ -10,8 +10,10 @@ def check_cronjob():
     try:
         my_cron = CronTab(user=True)
         cmd = sys.executable + " " + os.getcwd() + "/cron/cron.py"
-        if my_cron.find_command(cmd) is not None:
-            logging.info("Cronjob does already exist")
+        i = 0
+        for job in my_cron.find_command(cmd):
+            i += 1
+            logging.info("Cronjob does already exist: " + str(job))
             return True
 
         job = my_cron.new(cmd, 'Foodship API Worker')
